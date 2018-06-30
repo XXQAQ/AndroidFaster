d7f8bf11828cf71df8d8347c1592d33d3fac8288
xq <120363542@qq.com>
2018-06-30 10:43:03 +0800

    add isAutoPermission

--- library/src/main/java/com/xq/projectdefine/FasterInterface.java
+++ library/src/main/java/com/xq/projectdefine/FasterInterface.java
@@ -8,12 +8,23 @@ public class FasterInterface {
 
     private static String FILEPROVIDER;
 
+    private static boolean isAutoPermission;
+
+
+
     public static void init(Application app,String FILEPROVIDER){
+        init(app,FILEPROVIDER,true);
+    }
+
+    public static void init(Application app,String FILEPROVIDER,boolean isAutoPermission){
 
         FasterInterface.app = app;
         FasterInterface.FILEPROVIDER = FILEPROVIDER;
+        FasterInterface.isAutoPermission = isAutoPermission;
 
     }
+
+
 
     public static Application getApp() {
         return app;
@@ -23,4 +34,7 @@ public class FasterInterface {
         return FILEPROVIDER;
     }
 
+    public static boolean isIsAutoPermission() {
+        return isAutoPermission;
+    }
 }
