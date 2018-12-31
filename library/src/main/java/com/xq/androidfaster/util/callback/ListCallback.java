9c7cb03eac250825fd376605269f5e581e19d49e
XQ <120363542@qq.com>
2018-12-31 16:14:46 +0800

    删除多余的Callback

--- library/src/main/java/com/xq/androidfaster/util/callback/ListCallback.java
+++
@@ -1,9 +0,0 @@
-package com.xq.androidfaster.util.callback;
-
-import com.xq.androidfaster.bean.behavior.ListBehavior;
-
-public interface ListCallback {
-
-    public void onCallback(ListBehavior behavior);
-
-}
