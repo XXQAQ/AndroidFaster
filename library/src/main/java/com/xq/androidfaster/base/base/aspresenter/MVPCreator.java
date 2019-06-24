5a143764ca4c98869b739aa34fa11737445c5538
XXQAQ <120363542@qq.com>
2019-06-24 12:00:01 +0800

    ...

--- library/src/main/java/com/xq/androidfaster/base/base/aspresenter/MVPCreator.java
+++ library/src/main/java/com/xq/androidfaster/base/base/aspresenter/MVPCreator.java
@@ -8,9 +8,11 @@ import java.io.FileOutputStream;
 //一键生成MVP标准模板的工具。如需调整请自行修改，以下为示例代码
 //	new MVP(name).create();
 public class MVPCreator {
-	
+
+	public static final String packageName = "package " + "com.hlw.youlian.mvp.x.";
+
 	public static final String BASE ="Base";
-	
+
 	private String parents;
 	private String name;
 
@@ -33,74 +35,74 @@ public class MVPCreator {
 		this.parents = parents;
 		this.name = name;
 	}
-	
-	public void create() throws Exception {
-		
+
+	public void create() throws Exception{
+
 		String parent = String.format("%s", name).toLowerCase();
-		
-		FileUtil.writeData(createInterfacePresenter().getBytes(), parent+ File.separator+ String.format("I%sPresenter.java", name));
-		FileUtil.writeData(createActivity().getBytes(), parent+ File.separator+ String.format("%sActivity.java", name));
-		FileUtil.writeData(createFragment().getBytes(), parent+ File.separator+ String.format("%sFragment.java", name));
-		FileUtil.writeData(createInterfaceView().getBytes(), parent+ File.separator+ String.format("I%sView.java", name));
-		FileUtil.writeData(createView().getBytes(), parent+ File.separator+ String.format("%sView.java", name));
+
+		FileUtil.writeData(createInterfacePresenter().getBytes(), parent+File.separator+String.format("I%sPresenter.java", name));
+		FileUtil.writeData(createActivity().getBytes(), parent+File.separator+String.format("%sActivity.java", name));
+		FileUtil.writeData(createFragment().getBytes(), parent+File.separator+String.format("%sFragment.java", name));
+		FileUtil.writeData(createInterfaceView().getBytes(), parent+File.separator+String.format("I%sView.java", name));
+		FileUtil.writeData(createView().getBytes(), parent+File.separator+String.format("%sView.java", name));
 	}
-	
+
 	private String createInterfacePresenter(){
 
-		return String.format(IPRESENTER,name+"Presenter",parents+"Presenter",name+"View");
+		return String.format(IPRESENTER,name.toLowerCase(),name+"Presenter",parents+"Presenter",name+"View");
 	}
-	
+
 	private String createActivity(){
-		
-		return String.format(PRESENTER,name+"Activity",parents+"Activity",name+"View",name+"Presenter");
+
+		return String.format(PRESENTER,name.toLowerCase(),name+"Activity",parents+"Activity",name+"View",name+"Presenter");
 	}
-	
+
 	private String createFragment(){
 
-		return String.format(PRESENTER,name+"Fragment",parents+"Fragment",name+"View",name+"Presenter");
+		return String.format(PRESENTER,name.toLowerCase(),name+"Fragment",parents+"Fragment",name+"View",name+"Presenter");
 	}
-	
+
 	private String createInterfaceView(){
 
-		return String.format(IVIEW,name+"View",parents+"View",name+"Presenter");
+		return String.format(IVIEW,name.toLowerCase(),name+"View",parents+"View",name+"Presenter");
 	}
-	
+
 	private String createView(){
 
-		return String.format(VIEW,name+"View",parents+"View",name+"Presenter",name+"View");
+		return String.format(VIEW,name.toLowerCase(),name+"View",parents+"View",name+"Presenter",name+"View");
 	}
-	
+
 	static class FileUtil{
-		public static void writeData(byte []data, String path) throws Exception {
-			
+		public static void writeData(byte []data,String path) throws Exception{
+
 			File file = new File(path);
 
 			if (file.exists())
 			{
-				 throw new Exception("File is exists!");
+				throw new Exception("File is exists!");
 			}
 
 			if(!file.getParentFile().exists())
 			{
 				file.getParentFile().mkdirs();
 			}
-			
+
 			ByteArrayInputStream in=null;
 			BufferedOutputStream out=null;
-			try 
+			try
 			{
 				in=new ByteArrayInputStream(data);
 				out=new BufferedOutputStream(new FileOutputStream(file));
-				
+
 				byte []buffer = new byte[8*1024];
 				int length=0;
-				
+
 				while( (length = in.read(buffer)) != -1 )
 				{
 					out.write(buffer, 0, length);
 					out.flush();
 				}
-			} 
+			}
 			finally
 			{
 				if(in != null)
@@ -108,20 +110,20 @@ public class MVPCreator {
 				if(out != null)
 					out.close();
 			}
-				
+
 		}
 	}
 
-	private static String IPRESENTER = "public interface I%s extends I%s<I%s> {\n" +
+	private static String IPRESENTER = packageName+"%s;"+"\n\n"+"public interface I%s extends I%s<I%s> {\n" +
 			"}";
 
-	private static String PRESENTER = "public class %s extends %s<I%s> implements I%s {\n" +
+	private static String PRESENTER = packageName+"%s;"+"\n\n"+"public class %s extends %s<I%s> implements I%s {\n" +
 			"}";
 
-	private static final String IVIEW = "public interface I%s extends I%s<I%s> {\n" +
+	private static final String IVIEW = packageName+"%s;"+"\n\n"+"public interface I%s extends I%s<I%s> {\n" +
 			"}";
 
-	private static final String VIEW = "public class %s extends %s<I%s> implements I%s {\n" +
+	private static final String VIEW = packageName+"%s;"+"\n\n"+"public class %s extends %s<I%s> implements I%s {\n" +
 			"}";
 
 }
