18b3e74ab4c2cd5a728c59b17636915fcd53bcf1
XQ <120363542@qq.com>
2019-08-27 14:49:14 +0800

    ...

--- library/src/main/java/com/xq/androidfaster/AndroidFaster.java
+++ library/src/main/java/com/xq/androidfaster/AndroidFaster.java
@@ -1,19 +1,9 @@
 package com.xq.androidfaster;
 
-import android.app.Application;
-import com.xq.androidfaster.util.tools.Utils;
-
 public class AndroidFaster {
 
-    private static Application app;
+    public static void init(){
 
-    public static void init(Application app,String fileProvider){
-        AndroidFaster.app = app;
-        Utils.init(app,fileProvider);
-    }
-
-    public static Application getApp() {
-        return app;
     }
 
 }
