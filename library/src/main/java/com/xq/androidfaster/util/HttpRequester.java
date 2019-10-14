9e739c1acc7eff9700c1100cffd5481f6377fbb9
XQ <120363542@qq.com>
2019-10-14 10:28:04 +0800

    ...

--- library/src/main/java/com/xq/androidfaster/util/HttpRequester.java
+++ library/src/main/java/com/xq/androidfaster/util/HttpRequester.java
@@ -1,21 +1,20 @@
 package com.xq.androidfaster.util;
 
-import com.xq.androidfaster.util.callback.FasterHttpCallback;
 import java.util.Map;
 
 public class HttpRequester {
 
     private static Requester requester;
 
-    public static void get(String url, Map<Object, Object> map, FasterHttpCallback callback, Object... objects) {
+    public static void get(String url, Map<Object, Object> map, Callback callback, Object... objects) {
         requester.get(url,map,callback,objects);
     }
 
-    public static void post(String url, Map<Object, Object> map, FasterHttpCallback callback, Object... objects) {
+    public static void post(String url, Map<Object, Object> map, Callback callback, Object... objects) {
         requester.post(url,map,callback,objects);
     }
 
-    public static void postJson(String url, String json, FasterHttpCallback callback, Object... objects) {
+    public static void postJson(String url, String json, Callback callback, Object... objects) {
         requester.postJson(url,json,callback,objects);
     }
 
@@ -25,13 +24,46 @@ public class HttpRequester {
         HttpRequester.requester = requester;
     }
 
+    public static abstract class Callback<T>{
+
+        private T data;
+        private Class<T> dataClass;
+        private boolean isSuccess;
+
+        public Callback(Class<T> dataClass) {
+            this.dataClass = dataClass;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public void setSuccess(boolean success) {
+            isSuccess = success;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public Class<T> getDataClass() {
+            return dataClass;
+        }
+
+        public boolean isSuccess() {
+            return isSuccess;
+        }
+
+        public abstract void onCallback();
+    }
+
     public static abstract class Requester {
 
-        public abstract void get(String url, Map<Object,Object> map, FasterHttpCallback callback,Object... objects);
+        public abstract void get(String url, Map<Object,Object> map, Callback callback,Object... objects);
 
-        public abstract void post(String url, Map<Object,Object> map, FasterHttpCallback callback,Object... objects);
+        public abstract void post(String url, Map<Object,Object> map, Callback callback,Object... objects);
 
-        public abstract void postJson(String url, String json, FasterHttpCallback callback,Object... objects);
+        public abstract void postJson(String url, String json, Callback callback,Object... objects);
 
     }
 
