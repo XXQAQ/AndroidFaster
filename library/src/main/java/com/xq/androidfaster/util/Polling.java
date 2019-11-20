8ec3a5091efbb447ee7ed3ce5ddb23577e15ca0c
XQ <120363542@qq.com>
2019-11-20 17:48:23 +0800

    Polling新增暂停和恢复得方法

--- library/src/main/java/com/xq/androidfaster/util/Polling.java
+++ library/src/main/java/com/xq/androidfaster/util/Polling.java
@@ -4,56 +4,138 @@ import com.xq.androidfaster.util.tools.ThreadUtils;
 
 public class Polling {
 
-    private int millis;
     private Runnable run;
 
-    private Thread thread;
+    private CustomThread thread;
+
+    private int millis;
+    private int delay;
 
     public Polling(int millis, Runnable run) {
+        this(millis,0,run);
+    }
+
+    public Polling(int millis, int delay, Runnable run) {
         this.millis = millis;
+        this.delay = delay;
         this.run = run;
     }
 
-    private boolean isStart = false;
     public void start(){
         if (thread != null && thread.isAlive())
-            breakPolling();
+            destory();
 
-        thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                try {
-                    while (true)
-                    {
-                        Thread.sleep(millis);
-
-                        ThreadUtils.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                run.run();
-                            }
-                        });
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        thread = new CustomThread();
         thread.start();
-        isStart  = true;
+    }
+
+    public void resume(){
+        if (thread != null)
+            thread.resumeThread();
+    }
+
+    public void pause(){
+        if (thread != null)
+            thread.pauseThread();
+    }
+
+    public void destory(){
+        if (thread != null)
+            thread.interrupt();
     }
 
     public boolean isStart(){
-        return isStart;
+        if (thread != null){
+            return thread.isStart();
+        }
+        return false;
+    }
+
+    public boolean isPause(){
+        if (thread != null){
+            return thread.isPause();
+        }
+        return false;
     }
 
     public boolean isPolling(){
-        return thread != null && !thread.isInterrupted();
+        return thread != null && !thread.isInterrupted() && !isPause();
     }
 
-    public void breakPolling(){
-        if (thread != null)     thread.interrupt();
+    public class CustomThread extends Thread {
+
+        private final Object lock = new Object();
+
+        private boolean isFirstRun = true;
+
+        private boolean isStart = false;
+
+        private boolean isPause = false;
+
+        /**
+         * 调用该方法实现线程的暂停
+         */
+        public void pauseThread(){
+            isPause = true;
+        }
+
+        /**
+         * 调用该方法实现恢复线程的运行
+         */
+        public void resumeThread(){
+            isPause = false;
+            synchronized (lock){
+                lock.notify();
+            }
+        }
+
+        /**
+         * 这个方法只能在run 方法中实现，不然会阻塞主线程，导致页面无响应
+         */
+        private void onPause() throws InterruptedException {
+            synchronized (lock) {
+                lock.wait();
+            }
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            isStart = true;
+
+            while(true){
+                try {
+
+                    while (isPause){
+                        onPause();
+                    }
+
+                    Thread.sleep(isFirstRun?delay : millis);
+
+                    isFirstRun = false;
+
+                    ThreadUtils.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            run.run();
+                        }
+                    });
+                }catch (Exception e){
+                    e.printStackTrace();
+                    isPause = true;
+                    break;
+                }
+            }
+        }
+
+        public boolean isPause() {
+            return isPause;
+        }
+
+        public boolean isStart() {
+            return isStart;
+        }
     }
 
 }
