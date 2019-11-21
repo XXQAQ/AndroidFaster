4c1ee65e65970fc0a2c6afa4f7f57ac30868dfa0
XQ <120363542@qq.com>
2019-11-21 17:23:55 +0800

    ByteUtils新增对象字段与字节数组互转的方法

--- library/src/main/java/com/xq/androidfaster/util/tools/ByteUtils.java
+++ library/src/main/java/com/xq/androidfaster/util/tools/ByteUtils.java
@@ -1,9 +1,187 @@
 package com.xq.androidfaster.util.tools;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.net.DatagramPacket;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class ByteUtils {
+
+    private static Map<Class, Field[]> classMap = new HashMap<>();
+    public static byte[] objectField2Bytes(Object o) throws Exception {
+        if (o == null) return null;
+        return objectField2Bytes(o,o.getClass());
+    }
+
+    private static byte[] objectField2Bytes(Object o,Class mClass) throws Exception {
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(64*1000);
+
+        if (!classMap.containsKey(mClass)){
+            classMap.put(mClass,mClass.getDeclaredFields());
+        }
+
+        Field[] fields = classMap.get(mClass);
+
+        for (int i=0;i<fields.length;i++){
+
+            Field field = fields[i];
+
+            //设置强制访问
+            if (!field.isAccessible())
+                field.setAccessible(true);
+
+            // 静态字段 等情况不受理
+            if (Modifier.isStatic(field.getModifiers())){
+                continue;
+            }
+
+            if (field.getType().isArray()){
+
+                if (i == 0){
+                    throw  new Exception("what the fucking");
+                }
+
+                if (!(fields[i-1].getType().isPrimitive())){
+                    throw  new Exception(fields[i-1].getName() + " must be Number");
+                }
+
+                for (int j=0;j<((Number) fields[i-1].get(o)).intValue();j++){
+
+                    Object[] value = (Object[]) field.get(o);
+
+                    if (value == null){
+                        throw  new Exception(field.getName() + " is null");
+                    }
+
+                    byte[] bytes = objectField2Bytes(value[j],field.getType().getComponentType());
+                    if (bytes != null && bytes.length > 0)
+                        byteBuffer.put(bytes);
+                }
+            } else {
+
+                Object value = field.get(o);
+
+                if (field.getType().isAssignableFrom(long.class) || field.getType().isAssignableFrom(Long.class)){
+                    byteBuffer.putLong(value == null?0 : (Long) value);
+                }
+                else    if (field.getType().isAssignableFrom(int.class) || field.getType().isAssignableFrom(Integer.class)){
+                    byteBuffer.putInt(value == null?0 : (Integer) value);
+                }
+                else    if (field.getType().isAssignableFrom(short.class) || field.getType().isAssignableFrom(Short.class)){
+                    byteBuffer.putShort(value == null?0 : (Short) value);
+                }
+                else    if (field.getType().isAssignableFrom(char.class) || field.getType().isAssignableFrom(Character.class)){
+                    byteBuffer.putChar(value == null?0 : (Character) value);
+                }
+                else    if (field.getType().isAssignableFrom(byte.class) || field.getType().isAssignableFrom(Byte.class)){
+                    byteBuffer.put(value == null?0 : (Byte) value);
+                }
+                else    if (field.getType().isAssignableFrom(boolean.class) || field.getType().isAssignableFrom(Boolean.class)){
+                    byteBuffer.put(value == null?0 : (Boolean)value?(byte) 1:(byte) 0);
+                }
+                else    if (field.getType().isAssignableFrom(double.class) || field.getType().isAssignableFrom(Double.class)){
+                    byteBuffer.putDouble(value == null?0 : (Double) value);
+                }
+                else    if (field.getType().isAssignableFrom(float.class) || field.getType().isAssignableFrom(Float.class)){
+                    byteBuffer.putFloat(value == null?0 : (Float) value);
+                }
+                else    if (value != null){
+                    byteBuffer.put(objectField2Bytes(value,field.getType()));
+                }
+            }
+        }
+
+        return byteBuffer2Bytes(byteBuffer);
+
+    }
+
+    public static Object bytes2ObjectField(byte[] bytes,Class mClass) throws Exception{
+        return bytes2ObjectField(ByteBuffer.wrap(bytes),mClass);
+    }
+
+    private static Object bytes2ObjectField(ByteBuffer byteBuffer,Class mClass) throws Exception {
+        if (mClass == null)  return null;
+
+        Constructor constructor = mClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Object o = constructor.newInstance();
+
+        if (!classMap.containsKey(mClass)){
+            classMap.put(mClass,mClass.getDeclaredFields());
+        }
+
+        Field[] fields = classMap.get(mClass);
+
+        for (int i=0;i<fields.length;i++){
+
+            Field field = fields[i];
+
+            //设置强制访问
+            if (!field.isAccessible())
+                field.setAccessible(true);
+
+            // 静态字段 等情况不受理
+            if (Modifier.isStatic(field.getModifiers())){
+                continue;
+            }
+
+            if (field.getType().isArray()){
+
+                if (i == 0){
+                    throw  new Exception("what the fucking");
+                }
+
+                if (!(fields[i-1].getType().isPrimitive())){
+                    throw  new Exception(fields[i-1].getName() + " must be Number");
+                }
+
+                Object[] value = (Object[]) Array.newInstance(field.getType().getComponentType(),((Number) fields[i-1].get(o)).intValue());
+
+                field.set(o,value);
+
+                for (int j=0;j<((Number) fields[i-1].get(o)).intValue();j++){
+                    value[j] = bytes2ObjectField(byteBuffer,field.getType().getComponentType());
+                }
+            } else {
+
+                if (field.getType().isAssignableFrom(long.class) || field.getType().isAssignableFrom(Long.class)){
+                    field.set(o,byteBuffer.getLong());
+                }
+                else    if (field.getType().isAssignableFrom(int.class) || field.getType().isAssignableFrom(Integer.class)){
+                    field.set(o,byteBuffer.getInt());
+                }
+                else    if (field.getType().isAssignableFrom(short.class) || field.getType().isAssignableFrom(Short.class)){
+                    field.set(o,byteBuffer.getShort());
+                }
+                else    if (field.getType().isAssignableFrom(char.class) || field.getType().isAssignableFrom(Character.class)){
+                    field.set(o,byteBuffer.getChar());
+                }
+                else    if (field.getType().isAssignableFrom(byte.class) || field.getType().isAssignableFrom(Byte.class)){
+                    field.set(o,byteBuffer.get());
+                }
+                else    if (field.getType().isAssignableFrom(boolean.class) || field.getType().isAssignableFrom(Boolean.class)){
+                    field.set(o,byteBuffer.get() == (byte)1);
+                }
+                else    if (field.getType().isAssignableFrom(double.class) || field.getType().isAssignableFrom(Double.class)){
+                    field.set(o,byteBuffer.getDouble());
+                }
+                else    if (field.getType().isAssignableFrom(float.class) || field.getType().isAssignableFrom(Float.class)){
+                    field.set(o,byteBuffer.getFloat());
+                }
+                else {
+                    field.set(o,bytes2ObjectField(byteBuffer,field.getType()));
+                }
+            }
+        }
+
+        return o;
+
+    }
 
     public static byte[] concatBytes(byte[]... bytess){
         ByteBuffer byteBuffer = ByteBuffer.allocate(64*1024);
@@ -30,8 +208,8 @@ public final class ByteUtils {
     }
 
     public static byte[] byteBuffer2Bytes(ByteBuffer byteBuffer){
-        byte[] bytes = new byte[byteBuffer.limit()];
-        System.arraycopy(byteBuffer.array(),0,bytes,0,byteBuffer.limit());
+        byte[] bytes = new byte[byteBuffer.position()];
+        System.arraycopy(byteBuffer.array(),0,bytes,0,byteBuffer.position());
         return bytes;
     }
 
